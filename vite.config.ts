3455ef59728e806b190395ac650843b78226d7d3
takuma-ru <kanekotakuma@team-lab.com>
2024-04-08 17:39:33 +0900

    ⚡️ Enable formatter

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import {
-  vitePlugin as remix,
-  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+	vitePlugin as remix,
+	cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  server: {
-    port: 2222,
-  },
+	server: {
+		port: 2222,
+	},
 
-  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
+	plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
 });
