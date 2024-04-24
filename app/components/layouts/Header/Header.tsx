0cdfa03ed703390813fe1a5669f168c7fe363053
takuma-ru <kanekotakuma@team-lab.com>
2024-04-24 12:45:01 +0900

    ✨ sp用nav追加

--- app/components/layouts/Header/Header.tsx
+++ app/components/layouts/Header/Header.tsx
@@ -1,4 +1,10 @@
-import { type FC, Fragment } from "react";
+import {
+  type FC,
+  Fragment,
+  type TouchEvent,
+  useCallback,
+  useState,
+} from "react";
 
 import { NavLink } from "@remix-run/react";
 import { Divider } from "~/components/common/Divider/Divider";
@@ -28,23 +34,53 @@ const NAVIGATION = [
 export const Header: FC = () => {
   const { isSp } = useBreakpoint();
 
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = useCallback(() => {
+    setIsNavOpen((prev) => !prev);
+  }, []);
+
+  const handleClickItem = useCallback((e: TouchEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    setIsNavOpen(false);
+  }, []);
+
   return (
-    <header className={styles["header-component"]}>
-      {isSp && <MenuRounded fontSize="24px" />}
-      {!isSp && (
-        <div className={styles.navigation}>
-          {NAVIGATION.map((nav, index) => (
-            <Fragment key={nav.path}>
-              <NavLink to={nav.path} className={styles.link}>
-                {nav.label}
-              </NavLink>
-              {NAVIGATION.length > index + 1 && (
-                <Divider direction="vertical" />
-              )}
-            </Fragment>
-          ))}
-        </div>
-      )}
-    </header>
+    <>
+      <header className={styles["header-component"]}>
+        {isSp && <MenuRounded fontSize="24px" onClick={toggleNav} />}
+        {!isSp && (
+          <div className={styles.navigation}>
+            {NAVIGATION.map((nav, index) => (
+              <Fragment key={nav.path}>
+                <NavLink to={nav.path} className={styles.link}>
+                  {nav.label}
+                </NavLink>
+                {NAVIGATION.length > index + 1 && (
+                  <Divider direction="vertical" />
+                )}
+              </Fragment>
+            ))}
+          </div>
+        )}
+      </header>
+      <nav
+        className={styles["overlay-navigation"]}
+        style={{ display: isNavOpen ? "flex" : "none" }}
+        onTouchEnd={toggleNav}
+      >
+        {NAVIGATION.map((nav) => (
+          <NavLink
+            key={nav.path}
+            to={nav.path}
+            className={styles.item}
+            onTouchEnd={toggleNav}
+          >
+            {nav.label}
+          </NavLink>
+        ))}
+      </nav>
+    </>
   );
 };
--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#3d8cad" />
         <Meta />
         <Links />
       </head>
